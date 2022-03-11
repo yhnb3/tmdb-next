a453d0d1b8c818920d67aa1ced51e900eec59d7f
yhnb3 <60080270+yhnb3@users.noreply.github.com>
2022-03-11 22:45:57 +0900

    fix build error

--- pages/tv/[id].tsx
+++ pages/tv/[id].tsx
@@ -5,20 +5,25 @@ import axios from 'axios';
 import Layout from '../../components/Layout';
 import DetailPage from '../../components/detail/DetailPage';
 import { Content } from '../../components/detail/types'
+import isMobile from '../../libs/isMobile';
 
 interface Props {
-  data: Content
+  data: Content,
+  isMobileDevice: boolean
 }
 
-export default function TvDetail({data} : Props){
-  return <DetailPage content={data} />
+
+export default function TvDetail({data, isMobileDevice} : Props){
+  return <DetailPage content={data} isMobile={isMobileDevice}/>
 }
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
+  const isMobileDevice = isMobile(context.req)
   const data = await axios.get(`https://api.themoviedb.org/3/tv/${context.params.id}?api_key=${process.env.NEXT_PUBLIC_API_CODE}&language=ko`).then(res => res.data)
   return {
     props: {
-      data
+      data,
+      isMobileDevice
     }
   }
 }
