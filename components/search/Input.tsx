799c1c9296d0ce3d9ea4d633a9ea8e715536e9eb
yhnb3 <60080270+yhnb3@users.noreply.github.com>
2023-01-04 22:39:14 +0900

    Feat : repactor input to uncontrolled

--- components/search/Input.tsx
+++ components/search/Input.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, useState } from "react";
+import { useRef } from "react";
 
 import { FaSearch } from "@react-icons/all-files/fa/FaSearch";
 
@@ -7,12 +7,8 @@ interface IProps {
 }
 
 const Input = ({ query }: IProps) => {
-  const [inputValue, setInputValue] = useState(query);
-
-  const handleInputValue = (event: ChangeEvent<HTMLInputElement>) => {
-    const { value } = event.currentTarget;
-    setInputValue(value);
-  };
+  const queryString = query as string;
+  const inputRef = useRef();
 
   return (
     <div className="flex border-b">
@@ -22,13 +18,12 @@ const Input = ({ query }: IProps) => {
         </div>
         <form action="/search?" className="h-10 w-full">
           <input
+            ref={inputRef}
             className="text-gray-400 h-full outline-none"
             type="text"
             name="query"
             autoComplete="off"
-            placeholder="영화, tv 프로그램 검색..."
-            value={inputValue}
-            onChange={handleInputValue}
+            placeholder={queryString}
           />
         </form>
       </div>
